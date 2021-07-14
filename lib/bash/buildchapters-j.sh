f054535ea28bddf40c7964cbbdccb86076b1e4a8
Peter Mukunda Pasedach <p@muk.li>
2021-07-14 17:46:19 +0200

    Added buildchapters-j.sh

---
+++ lib/bash/buildchapters-j.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# We want to be in the directory in which the HV etext lives.  First we need to
+# remove the namespace declarations (find a better solution for that) from a
+# copy of the text that we'll actually be working from.
+
+cd ~/git/ratnakara-tei/haravijaya/all
+#for i in `echo 01 02 47`
+for i in `echo 01 03 06 47`
+do
+echo going to $i
+cd ../$i 
+pwd
+cat ../templates/template-beginning-j.xml > hv-$i-j.txt
+xmlstarlet sel -N x="http://www.tei-c.org/ns/1.0" -t -c "//x:div[@n=\"$i\"]" ../all/hv-all-j.xml >> hv-$i-j.txt
+cat ../templates/template-end.xml >> hv-$i-j.txt
+
+done
