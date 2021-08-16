7212a3d2702f480ea1899b47bb675dbf94047de1
Peter Mukunda Pasedach <p@muk.li>
2021-08-16 05:11:09 +0200

    Jodphur buildscript, small XML fixes.

---
+++ lib/bash/buildchapters-jodhpur.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# We want to be in the directory in which the HV etext lives.  First we need to
+# remove the namespace declarations (find a better solution for that) from a
+# copy of the text that we'll actually be working from.
+
+cd ~/git/ratnakara-tei/haravijaya/all
+for i in `echo 47`
+do
+echo going to $i
+cd ../$i 
+pwd
+cat ../templates/template-beginning-jodhpur.xml > hv-$i-jodhpur.txt
+xmlstarlet sel -N x="http://www.tei-c.org/ns/1.0" -t -c "//x:div[@n=\"$i\"]" ../all/hv-all-jodhpur.xml >> hv-$i-jodhpur.txt
+cat ../templates/template-end.xml >> hv-$i-jodhpur.txt
+
+done
