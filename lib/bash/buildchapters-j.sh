706f0fc59533b8d85613f054f37bb95fb5b32576
Peter Mukunda Pasedach <p@muk.li>
2022-04-26 09:55:28 +0200

    XML fix, added canto 39 to J buildscript.

--- lib/bash/buildchapters-j.sh
+++ lib/bash/buildchapters-j.sh
@@ -7,7 +7,7 @@
 cd ~/git/ratnakara-tei/haravijaya/all
 #for i in `echo 01 02 47`
 #for i in `echo 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 47`
-for i in `seq -w 37; echo 47`
+for i in `seq -w 39; echo 47`
 do
 echo going to $i
 cd ../$i 
