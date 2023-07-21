ae2657509bf265ba1f131a27ab5606744a64b102
Peter Mukunda Pasedach <p@muk.li>
2023-07-21 20:44:15 +0200

    ../../lib/bash/buildchapters.sh, added possibility to build a canto independently.

--- lib/bash/buildchapters.sh
+++ lib/bash/buildchapters.sh
@@ -10,139 +10,94 @@
 #ms="stein-189"
 #text="hvvu"
 
-function setvariables {
+function settextname {
 case $ms in
   stein-189)
-    cantos=(01 03)
     text="hvvu"
-    echo "I just did Stein 189."
     ;;
 
   stein-294)
-    cantos=(03)
     text="hvsv"
-    echo "I just did Stein 294."
     ;;
 
   bori-486-1887-91)
-    cantos=(03)
     text="hv"
-    echo "I just did bori-486-1887-91."
     ;;
 
   jammu-494-ga)
-    cantos=(03)
     text="hv"
-    echo "I just did jammu-494-ga."
     ;;
 
   jammu-797)
-    cantos=(03)
     text="hv"
-    echo "I just did jammu-797."
     ;;
 
   k-ocr)
-    cantos=(`seq -w 50`)
     text="hv"
-    echo "I just did k-ocr."
     ;;
 
   jammu-495)
-    cantos=(03)
     text="hvvu"
-    echo "I just did jammu-495."
     ;;
 
   bori-216-1875-76)
-    cantos=(03)
     text="hv"
-    echo "I just did bori-216-1875-76."
     ;;
 
   bori-227-a-1882-83)
-    cantos=(03)
     text="hvvu"
-    echo "I just did bori-227-a-1882-83."
     ;;
 
   bori-828-1886-72)
-    cantos=(03)
     text="hvvu"
-    echo "I just did bori-828-1886-72."
     ;;
 
   bori-486-1887-91)
-    cantos=(03)
     text="hv"
-    echo "I just did bori-486-1887-91."
     ;;
 
   j)
-    cantos=(01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 prasasti)
     text="hv"
-    echo "I just did Jaisalmer."
     ;;
 
   k)
-    cantos=(01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 prasasti)
     text="hv"
-    echo "I just did K."
     ;;
 
   bori-215-1875-76)
-    cantos=(01 03)
     text="hv"
-    echo "I just did bori-215-1875-76."
     ;;
 
   bori-217-1875-76)
-    cantos=(03)
     text="hvvu"
-    echo "I just did bori-217-1875-76."
     ;;
 
   rama)
-    cantos=(03)
     text="hv"
-    echo "I just did rama."
     ;;
 
   bombay)
-    cantos=(03)
     text="hv"
-    echo "I just did Bombay."
     ;;
 
   sn-757)
-    cantos=(01 02 06)
-    specialcantos=(03 04 05)
     text="hv"
-    echo "I just did SN-757."
     ;;
 
   p)
-    cantos=(01 02 03 06 47)
     text="hv"
-    echo "I just did P."
     ;;
 
   jodhpur)
-    cantos=(01 03 47)
     text="hv"
-    echo "I just did Jodhpur."
     ;;
 
   s-c)
-    cantos=(`seq -w 24; echo 47`)
     text="hv"
-    echo "I just did s-c."
     ;;
 
   stein-or-d72)
-    cantos=(03)
     text="hv"
-    echo "I just did stein-or-d72."
     ;;
 
   *)
@@ -151,6 +106,102 @@ case $ms in
 esac
 }
 
+function setdefaultcantos {
+case $ms in
+  stein-189)
+    cantos=(01 03)
+    ;;
+
+  stein-294)
+    cantos=(03)
+    ;;
+
+  bori-486-1887-91)
+    cantos=(03)
+    ;;
+
+  jammu-494-ga)
+    cantos=(03)
+    ;;
+
+  jammu-797)
+    cantos=(03)
+    ;;
+
+  k-ocr)
+    cantos=(`seq -w 50`)
+    ;;
+
+  jammu-495)
+    cantos=(03)
+    ;;
+
+  bori-216-1875-76)
+    cantos=(03)
+    ;;
+
+  bori-227-a-1882-83)
+    cantos=(03)
+    ;;
+
+  bori-828-1886-72)
+    cantos=(03)
+    ;;
+
+  bori-486-1887-91)
+    cantos=(03)
+    ;;
+
+  j)
+    cantos=(01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 prasasti)
+    ;;
+
+  k)
+    cantos=(01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 prasasti)
+    ;;
+
+  bori-215-1875-76)
+    cantos=(01 03)
+    ;;
+
+  bori-217-1875-76)
+    cantos=(03)
+    ;;
+
+  rama)
+    cantos=(03)
+    ;;
+
+  bombay)
+    cantos=(03)
+    ;;
+
+  sn-757)
+    cantos=(01 02 06)
+    specialcantos=(03 04 05)
+    ;;
+
+  p)
+    cantos=(01 02 03 06 47)
+    ;;
+
+  jodhpur)
+    cantos=(01 03 47)
+    ;;
+
+  s-c)
+    cantos=(`seq -w 24; echo 47`)
+    ;;
+
+  stein-or-d72)
+    cantos=(03)
+    ;;
+
+  *)
+    echo "Something wrong."
+    ;;
+esac
+}
 
 function build-beginning {
 echo "<TEI xmlns=\"http://www.tei-c.org/ns/1.0\">"  > $text-$i-$ms.txt
@@ -185,18 +236,23 @@ cat ../templates/template-end.xml >> $text-$i-$ms.txt
 done
 }
 
-while getopts am: opt
+while getopts am:c: opt
 do 
   case $opt in
     a) mss=(stein-189 stein-294 bori-486-1887-91 jammu-494-ga jammu-797 jammu-495 bori-216-1875-76 bori-215-1875-76 bori-217-1875-76 bori-227-a-1882-83 bori-828-1886-72 rama bombay p k j jodhpur k-ocr s-c);;
     m) mss="$OPTARG";;
+    c) cantos="$OPTARG";;
   esac
 done
 
 #function mainthing {
 for ms in ${mss[*]}
 do
-setvariables
+echo "Going to do $ms."
+echo "cantos = $cantos"
+#[[ ! -z "$cantos" ]] && echo "Testing Cantos is set" || setdefaultcantos
+[[ ! -z "$cantos" ]] || setdefaultcantos
+settextname
 build-chapters
 build-special-chapters
 done
